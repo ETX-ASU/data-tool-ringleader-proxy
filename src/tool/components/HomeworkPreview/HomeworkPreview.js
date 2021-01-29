222ab3b56a68812cc159bdd67b3943f8a0d3e791
Artur Wróbel <artur.gtg.wrobel@gmail.com>
2021-01-29 11:57:32 +0100

    feat: big chart in review mode

--- src/tool/components/HomeworkPreview/HomeworkPreview.js
+++ src/tool/components/HomeworkPreview/HomeworkPreview.js
@@ -1,26 +1,22 @@
 import React from "react";
 import { Chart } from "react-google-charts";
-import { Col, Row } from "react-bootstrap";
 import styles from "./HomeworkPreview.module.scss";
 
 export const HomeworkPreview = ({ data, chartType, chartOptions, observations }) => (
   <div className={styles.wrapper}>
-    <Row>
-      <Col className="col-7">
-        <div className={styles.chartPreview}>
-          <Chart
-            chartType={chartType}
-            data={data}
-            height="400px"
-            options={chartOptions}
-          />
-        </div>
-      </Col>
-      <Col className="col-5">
-        <div>
-          {observations}
-        </div>
-      </Col>
-    </Row>
+    <div className={styles.chartPreview}>
+      <Chart
+        chartType={chartType}
+        data={data}
+        options={{
+          ...chartOptions,
+          height: "500px",
+          width: undefined
+        }}
+      />
+    </div>
+    <div className={styles.observations}>
+      {observations}
+    </div>
   </div>
 )
