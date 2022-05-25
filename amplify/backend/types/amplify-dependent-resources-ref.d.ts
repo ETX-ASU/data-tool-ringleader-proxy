32ba1f44bd167272d098e1938ddd04dc1d6fd7d5
Filip Moroz <filip.moroz@gogoapps.io>
2022-05-25 19:47:09 +0200

    fix: added key directive to homework model schema

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,8 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "datatoolDB": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
